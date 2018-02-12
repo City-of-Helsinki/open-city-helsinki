edc79b2cc49a739d4511d56d0d327174f9535e73
Juho Rautio <juho.rautio@haltu.fi>
2018-02-12 14:20:32 +0200

    Added themes for homeview

--- src/config/colors.js
+++ src/config/colors.js
@@ -3,12 +3,16 @@ export type ColorSet = {
   max: string,
   med: string,
   min: string,
+  homefg: string,
+  homebg: string,
 }
 
 const colors: ColorSet = {
   max: '#000000',
   med: '#0000AA',
   min: '#FFFFFF',
+  homefg: '#ffc61e',
+  homebg: '#9fc9eb',
 };
 
 export default colors;
