5c5b78af1418ba3ad2ecba681cb84b815a44a1db
Niko Koli <niko.koli@haltu.fi>
2018-06-19 16:41:45 +0300

    Using latest tag from modules repo, added gradle.properties to .gitignore and removed unused imports from app.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,17 @@ import {
   DeviceEventEmitter,
 } from 'react-native';
 import { TabNavigator, TabBarBottom, StackNavigator, NavigationActions } from 'react-navigation';
-import { withProps } from 'recompose';
 import { initColors } from 'open-city-modules';
 import { translate } from 'react-i18next';
 import { generateToken } from 'src/utils/authentication_keys';
 import mapStyles from 'src/style';
-import SplashScreen from 'src/components/SplashScreen';
 import tabs from 'src/config/tabs';
-import Header from 'src/config/header';
-import CityChangeModal from 'src/components/CityChangeModal';
 import colors from 'src/config/colors';
 // eslint-disable-next-line no-unused-vars
 import i18n from 'src/config/translations';
 import heroBanner from '../img/main-hero-decoration.png';
 import linkedEventDecorator from '../img/main-image-decoration.png';
 import map_marker from '../img/marker_pin.png';
-import { captureMessage } from 'src/utils/sentryUtils';
-import { Sentry } from 'react-native-sentry';
 import Piwik from 'react-native-piwik'
 import CustomMapMarker from 'src/components/CustomMapMarker';
 
@@ -35,7 +29,6 @@ const tabBarOnPress = navigation => config => {
     jumpToIndex(scene.index)
   }
   Piwik.trackScreen(`/${scene.route.routeName}`, scene.route.routeName)
-
 };
 
 const navigationOptions = ({ navigation }) => {
@@ -102,31 +95,18 @@ async function tokenRequestListener(interfaceDeviceId) {
 class App extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-
-    this.state = {
-      modalVisible: false,
-    };
-    // $FlowFixMe
-    this.Header = withProps({
-      defaultRightAction: this.showModal,
-    })(Header);
   }
 
   componentWillMount() {
     DeviceEventEmitter.addListener('apdu', tokenRequestListener);
   }
 
-  showModal = () => this.setState({ modalVisible: true });
-  hideModal = () => this.setState({ modalVisible: false });
   render() {
     const screenProps = {
       colors,
       locale: 'fi',
-      // locale: this.props.i18n.language,
-      // Header: this.Header,
       homeViewBGColor: colors.fog,
       coatColor: colors.coat,
-      floatingButtonBGCOlor: colors.suomenlinna,
       heroBGColor: colors.summer,
       showHeader: false,
       separateHomeViewSections: true,
@@ -138,14 +118,13 @@ class App extends React.Component<Props, State> {
       showHero: true,
       showFeed: false,
       piwik: Piwik,
-      customMapMarker: CustomMapMarker
+      customMapMarker: CustomMapMarker,
     };
     return (
       <View style={{ flex: 1 }}>
         <MainStack
           screenProps={screenProps}
         />
-        <CityChangeModal visible={this.state.modalVisible} onClose={this.hideModal} />
       </View>
     );
   }
